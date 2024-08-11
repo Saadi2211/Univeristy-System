80ab53112bd1026b6d49fd264282d9ac63fe12dd
Saadi2211 <saadahmedggg@gmail.com>
2024-08-11 17:35:42 +0500

    assignment 1

---
+++ introoo.pl
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+Assalam o Alakium.
+My name is Muhammad Saad Ahmad.
+My father is a business he have a medical phrmacy.
+I live in Faisalabad and have a family of 5.
+I have done my 2 year degree in FSC(pre engineering).
+I am hardworking,quick learner,self-motivated and multi-tasker.
+I interseted your course AI Generative and learned some new skills.
+My short-term goal is get an online skil  and and hlep tp stable my family.
+I think PIAIC hlep us in my future.
+Thats all about me.
+Thank you.
+=======
+Assalam o Alakium.
+My name is Muhammad Saad Ahmad.
+My father is a business he have a medical phrmacy.
+I live in Faisalabad and have a family of 5.
+I have done my 2 year degree in FSC(pre engineering).
+I am hardworking,quick learner,self-motivated and multi-tasker.
+I interseted your course AI Generative and learned some new skills.
+My short-term goal is get an online skil  and and hlep tp stable my family.
+I think PIAIC hlep us in my future.
+Thats all about me.
+Thank you.
+>>>>>>> c9311a5ef689c252eab513216194011bf8a1b92a
